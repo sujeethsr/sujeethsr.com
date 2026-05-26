a33f1c709e58196f2c5cf8b9e9f810ad67a74f55
sujeethsr <sr.sujeeth21@gmail.com>
2026-05-25 21:51:02 -0500

    Update logo link to homepage

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -1,20 +1,17 @@
 // Server Component - No 'use client' directive for SEO benefits
 import React from 'react';
+import Link from 'next/link';
 import personalInfo from '@/data/personal-info.json';
 
-interface LogoProps {
-  readonly scrollToSection: (href: string) => void;
-}
-
-const Logo: React.FC<LogoProps> = ({ scrollToSection }) => {
+const Logo: React.FC = () => {
   return (
     <div className="flex-shrink-0">
-      <button 
-        onClick={() => scrollToSection('#hero')}
+      <Link
+        href="/"
         className="text-xl font-bold gradient-text font-mono"
       >
         {`<${personalInfo.personal.nickname} />`}
-      </button>
+      </Link>
     </div>
   );
 };
