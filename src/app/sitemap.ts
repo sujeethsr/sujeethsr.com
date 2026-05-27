622003226f02522c07a0ea77e10cc9c65f153057
sujeethsr <sr.sujeeth21@gmail.com>
2026-05-27 04:00:19 -0500

    Fix sitemap URLs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,9 @@
 import { MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://sujeethsr.vercel.app';
+  const baseUrl = (
+  process.env.NEXT_PUBLIC_SITE_URL || 'https://sujeethsr.vercel.app'
+).replace(/\/$/, '');
 
   return [
     {
